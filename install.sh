5cb89ca44fcf82442482b5a1f3e469651b8504be
Pedro Batista <pedrombat@hotmail.com>
2015-06-30 19:25:44 -0300

    Adicionado documentação em português. Faltando fazer correções e melhorias

--- install.sh
+++ install.sh
@@ -6,9 +6,13 @@
 ### Ubuntu 14.*
 ### Ubuntu 15.04
 
-echo "Check Ubuntu Kernel"
-
 ## Check ubuntu kernel to correcty 
+## kernel_version recebe uma linha do filtro do "uname -r | sed 's/\(\-[0-9]* ..." para saber
+## se o kernel é compativel com a instalação da PS4eye
+
+## se kernel_version for menor que kernel_reference, mostra uma mensagem de error e sai da instalação
+
+echo "Check Ubuntu Kernel"
 
 kernel_version=$(uname -r | sed 's/\(\-[0-9]*\-[a-zA-Z]*\)//')
 kernel_reference="3.16.0"
@@ -22,6 +26,10 @@ echo "Kernel Version $kernel_version"
 echo "Installing PS4eye camera"
 
 #Check if ps4eye is conected
+## ps4eye_conected recebe uma linha do filtro do "lsusb | grep 05a9:0580" para saber
+## se a camera esta plugada
+## se não receber nada, mostra uma mensagem de error e sai da instalação
+
 
 ps4eye_conected=$(lsusb | grep 05a9:0580)
 
@@ -33,6 +41,9 @@ fi
 echo "Detected ps4eye stereo camera -> $check_ps4eye_conected"
 
 #Check python-setuptools for install easy_install and pip -> pyusb
+## check_python_setuptools recebe uma linha do filtro do "dpkg -l | grep python-setuptools" para saber
+## se o python-setuptoos esta instalado
+## se não receber nada, intala o python-setuptoos
 
 check_python_setuptools=$(dpkg -l | grep python-setuptools)
 
@@ -42,6 +53,11 @@ fi
  
 echo  -e "\n Python-setuptools installed"
 
+#Check PIP installed
+## check_pip recebe uma linha do filtro do "which pip" para saber
+## se o pip esta instalado
+## se não receber nada, intala o pip
+ 
 
 check_pip=$(which pip)
 
@@ -53,6 +69,9 @@ fi
 echo  -e "\n pip and pyusb installed"
 
 #Check GIT installed
+## check_git_installed recebe uma linha do filtro do "dpkg -l | grep..." para saber
+## se se o git esta instalado
+## se não receber nada, intala o git
 
 check_git_installed=$(dpkg -l | grep -E ' git ')
 
@@ -63,6 +82,10 @@ fi
 echo  -e "\n GIT installed"
 
 #Check folder ps4eye
+## directory é onde esta o arquivo de instalação
+## se a pasta existe só mostra que ela existe
+## se não, faz um clone do repositório https://github.com/ps4eye/ps4eye 
+
 directory=$(pwd)
 if [ -d "$directory/ps4eye" ] ; then
 	echo -e "\nDirectory /ps4eye exists."
@@ -75,6 +98,9 @@ fi
 lsusb -d 05a9:0580
 
 #Initianting the ps4eye camera with python
+## Entra na pasta /ps4eye/python
+## inicia o arquivo ps4eye_init.py para configurar a ps4eye no sistema
+
 ls
 cd ps4eye/
 ls
